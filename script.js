c2bc435baa6180e3ebba5487e97349f8e5517fe8
danielSafonyan <danielsafonyan@gmail.com>
2022-12-22 19:51:08 +0000

    Feature: Rendering Searched Movies

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const movieList = document.getElementById('movie-list')
 
 const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`
 
+
 form.addEventListener('submit', handleMovieSearch)
 
 async function handleMovieSearch(event) {
@@ -13,35 +14,49 @@ async function handleMovieSearch(event) {
     const movieTitle = searchInput.value
     const resp = await fetch(`${API_URL}s=${movieTitle}&plot=short`)
     const data = await resp.json()
-    renderMovies(data.Search)
+    renderMoviesTemplate(data.Search)
+    renderMoviesInfo(data.Search)
 }
 
-function renderMovies(moviesArray) {
+function renderMoviesTemplate(moviesArray) {
     let moviesHtml = ""
     moviesArray.forEach(el => moviesHtml += getMovieHtml(el))
     movieList.innerHTML = moviesHtml
 }
 
+async function renderMoviesInfo(moviesArray) {
+    moviesArray.forEach(el => getMovieInfo(el))
+}
+
+async function getMovieInfo(movie) {
+    const resp = await fetch(`${API_URL}i=${movie.imdbID}`)
+    const data = await resp.json()
+    document.getElementById(`info-${movie.imdbID}`).innerHTML = getMovieInfoHtml(data)
+}
+
+function getMovieInfoHtml(movieObject) {
+    const {Title: title, Genre: genre, Year: year } = movieObject
+    const {Runtime: duration, imdbRating, Plot: plot} = movieObject
+    return `<div class="movie-title">${title}</div>
+            <div class="movie-genres">${genre}</div>
+            <div class="year-duration-rating">
+                <div class="movie-year">${year}</div>
+                <div class="movie-duration">${duration}</div>
+                <div class="movie-rating">
+                    <img src="./movie-star-icon.png" alt="" class="movie-star-icon">
+                    <div>${imdbRating}</div>
+                </div>
+            </div>
+            <div class="movie-description">${plot}</div>
+            <div class="add-watchlist"><img src="./add-watchlist.png" alt=""><div>Watchlist</div></div>`
+}
+
 function getMovieHtml(movieObject) {
-    console.log(movieObject)
-    const {Title, Poster, Year} = movieObject
+    const {Title: title, Poster: poster, imdbID} = movieObject
     return `<div class="movie-container">
-                        <img class="movie-poster" src="${Poster}" alt="movie poster image">
-                        <div class="movie-info">
-                            <div class="title-row-container">
-                                <div class="movie-title">${Title}</div>
-                                <div class="movie-rating">
-                                    <img src="./movie-star-icon.png" alt="" class="movie-star-icon">
-                                    <div>8.1</div>
-                                </div>
-                            </div>
-                            <div class="movie-year">${Year}</div>
-                            <div class="movie-duration">117 min</div>
-                            <div class="movie-genres">Action, Drama, Sci-fi</div>
-                            
-                            <div class="movie-description">A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find
-                            their creator.</div>
-                            <div class="add-watchlist"><img src="./add-watchlist.png" alt=""><div>Watchlist</div></div>
+                        <img class="movie-poster" src="${poster}" alt="movie poster image">
+                        <div class="movie-info" id="info-${imdbID}">
+                                <div class="movie-title">${title}</div>
                         </div>
                     </div>`
 }
