4ddbfb9bd7e33bd83163009037460410243bfe54
danielSafonyan <danielsafonyan@gmail.com>
2022-12-22 20:36:35 +0000

    Finished: Add to watchlist feature

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const form = document.querySelector('form')
 const movieList = document.getElementById('movie-list')
 
 const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`
-
+const watchList = localStorage.getItem('watchList') || [];
 
 form.addEventListener('submit', handleMovieSearch)
 
@@ -35,7 +35,7 @@ async function getMovieInfo(movie) {
 }
 
 function getMovieInfoHtml(movieObject) {
-    const {Title: title, Genre: genre, Year: year } = movieObject
+    const {Title: title, Genre: genre, Year: year , imdbID} = movieObject
     const {Runtime: duration, imdbRating, Plot: plot} = movieObject
     return `<div class="movie-title">${title}</div>
             <div class="movie-genres">${genre}</div>
@@ -48,15 +48,29 @@ function getMovieInfoHtml(movieObject) {
                 </div>
             </div>
             <div class="movie-description">${plot}</div>
-            <div class="add-watchlist"><img src="./add-watchlist.png" alt=""><div>Watchlist</div></div>`
+            <button class="watchlist add" data-imdbID="${imdbID}">Watchlist</button>`
 }
 
 function getMovieHtml(movieObject) {
     const {Title: title, Poster: poster, imdbID} = movieObject
     return `<div class="movie-container">
-                        <img class="movie-poster" src="${poster}" alt="movie poster image">
+                        <img class="movie-poster" src="${poster}" alt="${title} movie poster">
                         <div class="movie-info" id="info-${imdbID}">
                                 <div class="movie-title">${title}</div>
                         </div>
                     </div>`
+}
+
+document.addEventListener('click', handleClick)
+
+function handleClick(event) {
+    const dataSet = event.target.dataset
+    if (dataSet.imdbid) {
+        if (watchList.includes(dataSet.imdbid)) {
+            console.log("Duplicate")
+            return
+        }
+        watchList.push(dataSet.imdbid)
+        console.log(watchList)
+    }
 }
